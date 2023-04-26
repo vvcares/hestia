59867e7e66126a0c4c36d1385d43acd87127961b
VVCARES <vvcares@users.noreply.github.com>
2023-04-26 10:53:31 +0530

    Update hestia_csf_install.sh

--- hestia_csf_install.sh
+++ hestia_csf_install.sh
@@ -6,13 +6,10 @@ DIR="/etc/csf/"
 T=$(date +"%m%d%Y%H%M%S")
 BKP=/etc/CSF-$T
 PANEL=/usr/local/hestia/web/templates/includes/panel.php
+PANEL2=/usr/local/hestia/web/list/csf
 HESTIAPORT="$BACKEND_PORT"
 CSFCONF='/etc/csf/csf.conf'
-
-source /usr/local/hestia/conf/hestia.conf
-HESTIAPORT="$BACKEND_PORT"
-#echo $HESTIAPORT
-
+HESTIAPORT='source /usr/local/hestia/conf/hestia.conf | echo $BACKEND_PORT'
 
 if [ -d "$DIR" ]; then
   echo "Existing CSF folder detected & skip new CSF install & proceeding to config hestia panel navigation"
@@ -21,20 +18,24 @@ if [ -d "$DIR" ]; then
   sudo apt update -y && apt-get install libwww-perl -y && cd /usr/src && rm -fv csf.tgz && wget https://download.configserver.com/csf.tgz && tar -xzf csf.tgz && cd csf && sudo sh install.sh && sudo csf -v && perl /usr/local/csf/bin/csftest.pl
 fi
 
-#IF old conf file exists..
-#[ -f "$BKP/csf.conf" ] && mv $CSFCONF $CSFCONF-OLD && cp $BKP/csf.conf $DIR
+#Setting up hestia folders
+mkdir -v -m 0600 $PANEL2
+cp -R /etc/csf/ui/images $PANEL2
+find $PANEL2 -type f -exec chmod -v 644 {} \;
+
+rm -f /usr/local/hestia/bin/csf.pl*
+wget https://raw.githubusercontent.com/vvcares/hestia/master/hestia-csf/csf.pl -P /usr/local/hestia/bin/
+chmod 700 /usr/local/hestia/bin/csf.pl
+#############
 
 cp $CSFCONF $CSFCONF-BKP-$T
 #zip -r $BKP.zip $DIR
-
 
 #Add Hestia port into CSF TCP_IN
 sed -i '/TCP_IN = "'$HESTIAPORT'/!s/TCP_IN = "/TCP_IN = "'$HESTIAPORT,'/' $CSFCONF
 #nano $CSFCONF
 
-
-
-# Add the CSF navigation link to panel top right
+# Add the CSF navigation link into panel top right
 if grep -q 'CSF' $PANEL; then
 echo 'This Link Is Already there.'
 else
